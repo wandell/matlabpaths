cb7d80d482f55d9b6092a3c74694ed45013d0c85
Brian Wandell <wandell@stanford.edu>
2019-03-25 09:27:26 -0700

    Should be shared, too

---
+++ updateGit.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+#
+# Script to git pull multiple repositories
+#
+# Could add the Flywheel repositories, too.
+
+#ISET
+echo "*********"
+echo "iset"
+echo "*********"
+
+cd ~/Documents/MATLAB/isetcam
+pwd
+git pull
+
+cd ~/Documents/MATLAB/iset3d
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetcam/isetcam.wiki
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetbio
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetfluorescence
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetbio/isetbio.wiki
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetcalibrate
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetauto
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetlens
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetcloud
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetL3
+pwd
+git pull
+
+cd ~/Documents/MATLAB/isetmosaics
+pwd
+git pull
+
+# Teach
+echo "*********"
+echo "Teach"
+echo "*********"
+
+cd ~/Documents/MATLAB/teach/psych221
+pwd
+git pull
+
+cd ~/Documents/MATLAB/teach/teachmri
+pwd
+git pull
+
+# Tools
+echo "*********"
+echo "Tools"
+echo "*********"
+
+cd ~/Documents/MATLAB/tools/JSONio
+pwd
+git pull
+
+cd ~/Documents/MATLAB/tools/RemoteDataToolbox
+pwd
+git pull
+
+cd ~/Documents/MATLAB/tools/UnitTestToolbox
+pwd
+git pull
+
+# LABS
+echo "*********"
+echo "LABS"
+echo "*********"
+
+cd ~/Documents/MATLAB/LABS/WL/WLVernierAcuity
+pwd
+git pull
+
+cd ~/Documents/MATLAB/LABS/WL/WLEyeMovementsCSF
+pwd
+git pull
+
+cd ~/Documents/MATLAB/LABS/WL/WLDiscriminationNetwork
+pwd
+git pull
+
+cd ~/Documents/MATLAB/LABS/WL/WLDichromacyAppearance
+pwd
+git pull
+
+cd ~/Documents/MATLAB/LABS/WL/huaquad
+pwd
+git pull
+
+cd ~/Documents/MATLAB/LABS/WL/jedeye
+pwd
+git pull
+
+# cd ~/Documents/MATLAB/LABS/BL/IBIOColorDetect
+# pwd
+# git pull
+
+cd ~/Documents/MATLAB/LABS/WL/oraleye
+pwd
+git pull
+
+# Publications
+echo "*********"
+echo "papers"
+echo "*********"
+# cd ~/Documents/MATLAB/papers/IBIOColorDetect_CSFpaper
+# pwd
+# git pull
+
+
+#MRI
+echo "*********"
+echo "MRI"
+echo "*********"
+
+cd ~/Documents/MATLAB/vistasoft
+pwd
+git pull
+
+cd ~/Documents/MATLAB/vistasoft/wiki
+pwd
+git pull
+
+cd ~/Documents/MATLAB/MRI/life
+pwd
+git pull
+
+# Scitran
+echo "Scitran"
+
+cd ~/Documents/MATLAB/scitran
+pwd
+git pull
+
+cd ~/Documents/MATLAB/scitran/scitran.wiki
+pwd
+git pull
+
+cd ~/Documents/MATLAB/scitranApps/dti-error
+pwd
+git pull
+
+cd ~/Documents/MATLAB/scitranApps/pof_Rorie2010
+pwd
+git pull
+
+cd ~/Documents/MATLAB/scitranApps/ecogBasicCode
+pwd
+git pull
+
+
+
+echo "Done"
+#
+
